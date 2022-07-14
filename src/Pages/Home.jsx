bd7c79255623f314732ec24bafa229b2efa1bdce
omar-faruuk <omarfaruq.anon@gmail.com>
2022-07-15 02:32:43 +0600

    initial push

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import { FacebookOutlined } from '@mui/icons-material';
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>HOME</h1>
+            <FacebookOutlined/>
+        </div>
+    );
+};
+
+export default Home;
